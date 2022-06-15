7d286ce53a34db054db7639c92e1d0c9f15a293b
turki-alotaibi9 <107382355+turki-alotaibi9@users.noreply.github.com>
2022-06-15 14:38:49 +0300

    Add files via upload

---
+++ homework3.dart
@@ -0,0 +1,18 @@
+import 'package:homework3/homework3.dart' as homework3;
+
+void main(List<String> arguments) {
+  int id=123;
+  String name="turki";
+  double gpa=4.99;
+  var language="dart";
+  List<String> list=["turki","mohammed","saad",];
+  Map map={"name":"turki","age":20};
+
+  print(id);
+  print(name);
+  print(gpa);
+  print(language);
+  print('this is list ${list}');
+  print("this is map ${map}");
+  
+  }
